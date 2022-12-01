34afe5331b27f78d2ff029fef205dca3d531a904
Valentin Nemcev <valentin.nemcev@smartly.io>
2022-12-01 22:05:05 +0200

    task 1 first

--- main.ts
+++ main.ts
@@ -1,8 +1,23 @@
-export function add(a: number, b: number): number {
-  return a + b;
+export function task1(input: string): number {
+  const chunks: number[][] = [];
+  let nextChunk: number[] = [];
+  input.split("\n").forEach((line) => {
+    if (line == "") {
+      if (nextChunk.length > 0) {
+        chunks.push(nextChunk);
+        nextChunk = [];
+      }
+    } else {
+      nextChunk.push(parseInt(line));
+    }
+  });
+  const sums = chunks.map((chunk) => chunk.reduce((a, b) => a + b));
+  sums.sort((a, b) => b - a);
+  return sums[0];
 }
 
 // Learn more at https://deno.land/manual/examples/module_metadata#concepts
 if (import.meta.main) {
-  console.log("Add 2 + 3 =", add(2, 3));
+  const input1 = await Deno.readTextFile("input/1.txt");
+  console.log(task1(input1));
 }
--- main_test.ts
+++ main_test.ts
@@ -1,6 +1,25 @@
 import { assertEquals } from "https://deno.land/std@0.166.0/testing/asserts.ts";
-import { add } from "./main.ts";
 
-Deno.test(function addTest() {
-  assertEquals(add(2, 3), 5);
+import heredoc from "npm:theredoc";
+
+import { task1 } from "./main.ts";
+
+Deno.test(function testTask1() {
+  const input = heredoc`
+    1000
+    2000
+    3000
+    
+    4000
+    
+    5000
+    6000
+    
+    7000
+    8000
+    9000
+    
+    10000
+  `;
+  assertEquals(task1(input), 24000);
 });
